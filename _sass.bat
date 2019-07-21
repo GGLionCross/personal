92f8c1fcd9950b2c1e0a1937b3e4e77a213ce0b7
GGLionCross <lionelquiambao@gmail.com>
2019-07-20 17:48:34 -0700

    Added _navigation.scss

--- _sass.bat
+++ _sass.bat
@@ -1,1 +1,4 @@
-sass --watch --style=compressed --no-source-map src/scss/main.scss:dist/main.min.css
+@ECHO OFF
+IF "%1" == "" sass --style=compressed --no-source-map src/scss/main.scss:dist/main.min.css
+IF "%1" == "w" sass --watch --style=compressed --no-source-map src/scss/main.scss:dist/main.min.css
+IF NOT "%2" == "" ECHO ONE ARG MAX
--- src/js/components/presentational/Navigation.jsx
+++ src/js/components/presentational/Navigation.jsx
@@ -4,10 +4,14 @@ import { Navbar, Nav } from "react-bootstrap";
 const Navigation = () => (
   <Navbar bg="primary" variant="dark">
     <Nav>
-      <Nav.Link onSelect={() => console.log("Active Page: Projects")} href="#projects">Projects</Nav.Link>
-      <Nav.Link onSelect={() => console.log("Active Page: Skills")} href="#skills">Skills</Nav.Link>
-      <Nav.Link onSelect={() => console.log("Active Page: Education")} href="#education">Education</Nav.Link>
+      <Nav.Link onSelect={() => active("projects")} href="#projects">Projects</Nav.Link>
+      <Nav.Link onSelect={() => active("skills")} href="#skills">Skills</Nav.Link>
+      <Nav.Link onSelect={() => active("education")} href="#education">Education</Nav.Link>
     </Nav>
   </Navbar>
 );
+
+function active(page) {
+  console.log("Active Page: ", page);
+}
 export default Navigation;
