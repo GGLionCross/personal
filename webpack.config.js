01d4546166552209e77d8d00e7d37fb11142d78d
GGLionCross <lionelquiambao@gmail.com>
2019-08-01 22:20:30 -0700

    Configured webpack to build to docs/ instead of dist/

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const path = require('path');
 
 module.exports = {
+  output: {
+    path: path.join(__dirname, 'docs')
+  },
   module: {
     rules: [
       {
@@ -42,7 +45,7 @@ module.exports = {
     })
   ],
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    contentBase: path.join(__dirname, 'docs'),
     compress: true,
     port: 3000
   }
