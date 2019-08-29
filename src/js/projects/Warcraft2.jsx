8375b2f1042fcfb85e56e0d94d225e1655c5ac26
GGLionCross <lionelquiambao@gmail.com>
2019-08-29 13:39:25 -0700

    Working on the Warcraft 2 presentation

--- src/js/projects/Warcraft2.jsx
+++ src/js/projects/Warcraft2.jsx
@@ -1,13 +1,29 @@
 import React, { Component } from "react";
 import ProjectCard from "../templates/ProjectCard.jsx";
 import { Table, Button, Modal } from "react-bootstrap";
-import display from "../../img/krona.png";
-class Warcraft2 extends ProjectCard {
+import display from "../../img/warcraft-2.png";
+class Krona extends ProjectCard {
   projectDisplay() {
-    return <img style={{ backgroundColor: "red" }}/>;
+    return <img src={display}/>;
   }
   projectTitle() {
     return <h1 className="text-bold text-32">Warcraft 2</h1>;
+  }
+  projectSummary() {
+    return (
+      <div>
+        <p>As a <span className="text-bold">programmer</span>, I assisted with graphical user interface (GUI), designing the minimap, sound menu, and networking menu.</p>
+      </div>
+    );
+  }
+  projectDescription() {
+    return (
+      <div id="krona-description">
+        <Button variant="primary" href="https://github.com/GGLionCross/personal/blob/master/proj/Krona_v0.5.8_exe.zip?raw=true">
+          Test the game
+        </Button>
+      </div>
+    );
   }
 }
 export default Warcraft2;
