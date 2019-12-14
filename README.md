2dd3e2a1f337e97254525a603bf557cce5f9e52e
GGLionCross <lionelquiambao@gmail.com>
2019-12-14 13:05:16 -0800

    .

--- README.md
+++
@@ -1,6 +0,0 @@
-# personal
-My personal website
-
-# notes
-Run Live Server with command "npm start"
-Build docs folder from src folder with "npm run build"
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./scss/main.scss";
-import App from "./js/App.jsx";
-const root = document.getElementById("root");
-root ? ReactDOM.render(<App/>, root) : false;
